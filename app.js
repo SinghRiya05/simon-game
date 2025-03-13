8718922ff8e4f37c03470475297f03e15c3c9348
SinghRiya05 <riyasingh969641@gmail.com>
2025-03-13 17:03:55 +0530

    changed

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ let body=document.querySelector("body");
 let started=false;
 let level=0;
 let h2=document.querySelector("h2");
+let clk=document.querySelector(".start");
+
+clk.addEventListener("click",function(){
+    if(started==false){
+        console.log("game start");
+        started=true;
+
+        levelUp();
+    }
+});
 
 document.addEventListener("keypress",function(){
     if(started==false){
@@ -43,7 +53,7 @@ function levelUp(){
 level++;
 h2.innerText=`Level ${level}`;
 
-let randIdx=Math.floor(Math.random()*3);
+let randIdx=Math.floor(Math.random()*4);
 let randColor=btns[randIdx];
 let randbtn=document.querySelector(`.${randColor}`);
 gaemSeq.push(randColor);
